911ad41a26660f1eaaa80ed932f551ef636aa389
kenneth steven chapman <845869531@kent-202-04m.local>
2017-04-10 08:50:54 +1200

    burger fix javascript

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
 
 
-  $('#burger-container').click(function(){
-    $('#slide').show();
-	});
-
+  // $('#burger-container').click(function(){
+  //   $('#slide').show();
+	// });
+  //
   $('body li a').click(function(){
     $('#slide').hide();
     $('#burger-container').toggleClass("open");
 	});
 
+  // $('#burger-container.open').click(function(){
+  //   alert("this is working");
+  //   $('#slide').hide();
+  // });
+
   $("#burger-container").on('click', function(){
     $(this).toggleClass("open");
+    $('#slide').toggle();
   });
 
-  $('open').click(function(){
-    $('#slide').hide();
-	});
+
 
 });
